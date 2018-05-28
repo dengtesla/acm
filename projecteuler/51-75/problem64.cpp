a2d422c33a757f6f688a03369287374f10d67b02
dengtesla <dengtesla@163.com>
2018-05-28 23:06:23 +0800

    64 is done

---
+++ projecteuler/51-75/problem64.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> a;
+vector<int> b;
+vector<int> c;
+
+void get_fractions(int n)
+{
+    a.clear();
+    b.clear();
+    c.clear();
+    int AA;
+    AA = floor(sqrt(n));
+    int c0 = n-AA*AA;
+    int a0 = (sqrt(n)+AA)/(n-AA*AA);
+    int b0 = a0*(n-AA*AA)-AA;
+    c.push_back(c0);
+    a.push_back(a0);
+    b.push_back(b0);
+    int i=0;
+    do
+    {
+        int ccc = (n - b[i]*b[i])/c[i];
+        int aaa = (sqrt(n)+b[i])/ccc;
+        int bbb = aaa*ccc-b[i];
+        if(a[0]==aaa&&b[0]==bbb&&c[0]==ccc)
+            break;
+        c.push_back(ccc);
+        a.push_back(aaa);
+        b.push_back(bbb);
+        i++;
+    }while(1);
+    /*
+    printf("[%d;(",AA);
+    vector<int>::iterator it;
+    for(it=a.begin();it!=a.end();it++)
+        cout<<*it<<(it==a.end()-1?")]":",");
+    //for(auto& x : a) cout << x;
+    cout << endl;
+    */
+}
+
+int main()
+{
+    int cnt=0;
+    for(int i=1;i<=10000;i++)
+    {
+        int k = sqrt(i);
+        if(k*k!=i)
+        {
+            get_fractions(i);
+            if(a.size()%2==1)
+            {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
+
